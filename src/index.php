eda57203a107b75fcb0b9358af33e8d3430402a2
Cristian Bastidas <gabrielbastidas5g@outlook.com>
2023-04-08 00:32:35 -0500

    index: list of methods done

--- src/index.php
+++ src/index.php
@@ -100,10 +100,192 @@
             <div class="card">
                 <a href="integrate_gauss_quadrature.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
                     <img src="assets/legendre.png" alt="twbs" width="64" height="64" class="flex-shrink-0">
-                    <div class="d-flex gap-2 w-100 justify-content-between">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
                         <div>
                             <h6 class="mb-0">Integración numérica: Cuadratura de Gauss</h6>
                             <p class="mb-0 opacity-75">Integración numérica por cuadratura de Gauss dado el límite inferior, superior una tolerancia y el grado del polinomio.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="lu_decomposition_gaussian_elimination.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/gaussian_elimination.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Eliminación Gaussiana y Factorización LU</h6>
+                            <p class="mb-0 opacity-75">Obtiene las soluciones de un sistema de ecuaciones a través de la Eliminación Gaussiana. Además muestra la factorización LU.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="thomas.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Método de Thomas para matrices tridiagonales</h6>
+                            <p class="mb-0 opacity-75">Obtiene las soluciones de un sistema de ecuaciones a través del método de Thomas para matrices tridiagonales.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="gauss_jacobi_seidel.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Gauss-Jacobi y Seidel</h6>
+                            <p class="mb-0 opacity-75">Obtiene las soluciones de un sistema de ecuaciones a través de los métodos de Gauss-Jacobi o Seidel.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="over_relaxation.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Relajaciones sucesivas</h6>
+                            <p class="mb-0 opacity-75">Obtiene las soluciones de un sistema de ecuaciones a través del método de relajaciones sucesivas.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="qr_decomposition.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Factorización QR</h6>
+                            <p class="mb-0 opacity-75">Factorización QR de matrices.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="root_finding.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Todos los métodos de búsqueda de raíces</h6>
+                            <p class="mb-0 opacity-75">Búsqueda de raíces por todos los métodos mostrados anteriormente.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="quadratic_fitting.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Ajuste de curvas por método cuadrático</h6>
+                            <p class="mb-0 opacity-75">Ajusta una serie de puntos a curvas cuadráticas.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="cubic_splines.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Splines cúbicos</h6>
+                            <p class="mb-0 opacity-75">Ajusta una serie de puntos con el método de splines cúbicos. Se debe proporcionar un archivo con los puntos. Permite dos métodos para la solución de sistemas de ecuadiones.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="linear_lagrange_quadratic.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Ajuste de curvas lineal, cuadrático y lagrange</h6>
+                            <p class="mb-0 opacity-75">Ajusta una serie de puntos de forma lineal, cuadrática o usando la interpolación de lagrange.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="edo_euler.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Método de Euler para solución de ecuación diferenciales</h6>
+                            <p class="mb-0 opacity-75">Solución de ecuaciones diferenciales por el método de Euler. Se especifican la función de dos variables y las aproximaciones iniciales.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="edo_euler_improved.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Método de Euler mejorado para solución de ecuación diferenciales</h6>
+                            <p class="mb-0 opacity-75">Solución de ecuaciones diferenciales por el método de Euler. Se especifican la función de dos variables y las aproximaciones iniciales.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+
+
+            <div class="card">
+                <a href="edo_euler_modified.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Método de Euler modificado para solución de ecuación diferenciales</h6>
+                            <p class="mb-0 opacity-75">Solución de ecuaciones diferenciales por el método de Euler. Se especifican la función de dos variables y las aproximaciones iniciales.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="rugen_kutta.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Método de Rugen Kutta para solución de ecuaciones diferenciales</h6>
+                            <p class="mb-0 opacity-75">Solución de ecuaciones diferenciales por el método de Euler. Se especifican la función de dos variables y las aproximaciones iniciales.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="linear_multistep_method.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Método de múltiples pasos para solución de ecuaciones diferenciales</h6>
+                            <p class="mb-0 opacity-75">Solución de ecuaciones diferenciales por el método de Euler. Se especifican la función de dos variables y las aproximaciones iniciales.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="multivariable_newton.php" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+                    <img src="assets/tridiagonal.jpg" alt="twbs" width="64" height="64" class="flex-shrink-0">
+                    <div class="d-flex gap-2 w-1lo00 justify-content-between">
+                        <div>
+                            <h6 class="mb-0">Método de Newton para funciones multivariables</h6>
+                            <p class="mb-0 opacity-75">Solución de ecuaciones diferenciales por el método de Euler. Se especifican la función de dos variables y las aproximaciones iniciales.</p>
                         </div>
                     </div>
                 </a>
